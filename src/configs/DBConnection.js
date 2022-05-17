429bd37b24d56ab01c46e049b96b6a2ac4f4320d
Mark-GeorgeK <79061147+Mark-GeorgeK@users.noreply.github.com>
2022-05-17 18:07:33 +0200

    debugging messages

--- src/configs/DBConnection.js
+++ src/configs/DBConnection.js
@@ -9,7 +9,10 @@ let connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
+    if (err) {
+        console.log(err);
+        throw err;
+    }
     console.log("Database connected!");
 });
 
